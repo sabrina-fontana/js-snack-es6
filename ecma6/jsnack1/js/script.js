5ea2845dba18da614b1539bdad2ec5ed2046772b
sabrina-fontana <sabfon@hotmail.it>
2021-01-19 16:45:35 +0100

    correzione esercizi

--- ecma6/jsnack1/js/script.js
+++ ecma6/jsnack1/js/script.js
@@ -1,32 +1,37 @@
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore utilizzando destructuring e template literal (backtick `)
-var arrayBici = [
+const arrayBici = [
   {
     nome: 'bici1',
     peso: 30
   },
   {
     nome: 'bici2',
-    peso: 50
+    peso: 16
   },
   {
     nome: 'bici3',
-    peso: 40
+    peso: 22
   },
   {
     nome: 'bici4',
-    peso: 70
+    peso: 18
   }
 ];
 
+// ipotizzo che la bici più leggera sia la prima dell'array
 let biciLeggera = arrayBici[0];
 
+// ciclo nell'array per trovare una bici più leggera (se esiste)
 for (let x = 0; x < arrayBici.length; x++) {
   if (arrayBici[x].peso < biciLeggera.peso) {
     biciLeggera = arrayBici[x];
   }
 }
 
-let {nome, peso} = biciLeggera;
+// destructuring
+const {nome, peso} = biciLeggera;
+
+// template literals
 console.log(`La bici con peso minore è ${nome}, e il suo peso è ${peso}kg`);
--- ecma6/jsnack2/js/script.js
+++ ecma6/jsnack2/js/script.js
@@ -1,9 +1,7 @@
 // Creare un array di oggetti di squadre di calcio.
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
-// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-let squadre = [
+const squadre = [
   {
     nome: 'Inter',
     punti: 0,
@@ -31,21 +29,29 @@ let squadre = [
   }
 ];
 
-let nuovoSquadre = [{}, {}, {}, {}, {}];
+const nuovoSquadre = [];
 
 function randomNumber(min, max) {
   let result = Math.floor(Math.random() * (max + 1 - min) + min);
   return result;
 }
 
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti
 for (let x = 0; x < squadre.length; x++) {
-  let punti = randomNumber(0, 20);
-  let falli = randomNumber(0, 10);
-  squadre[x].punti = punti;
-  squadre[x].falliSubiti = falli;
-  let {nome, falliSubiti} = squadre[x];
-  nuovoSquadre[x].nome = nome;
-  nuovoSquadre[x].falliSubiti = falliSubiti;
+  squadre[x].punti = randomNumber(0, 20);
+  squadre[x].falliSubiti = randomNumber(0, 10);
 }
+
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+for (let x = 0; x < squadre.length; x++) {
+  const {nome, falliSubiti} = squadre[x];
+  nuovoSquadre.push(
+    {
+      nome,
+      falliSubiti
+    }
+  );
+}
+
 console.log(squadre);
 console.log(nuovoSquadre);
