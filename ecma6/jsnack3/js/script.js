b3dd56e28b0760361ade299e5e646b5831568bc4
sabrina-fontana <sabfon@hotmail.it>
2021-01-20 16:52:18 +0100

    esercizio corretto con controlli

--- ecma6/jsnack3/js/script.js
+++ ecma6/jsnack3/js/script.js
@@ -1,14 +1,20 @@
-// Si scriva una funzione che accetti tre argomenti, un array e due numeri (A più piccolo di B).
-// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri
+// *****variabili*****
+var arrayNomi = ['qui', 'quo', 'qua', 'pippo', 'pluto', 'paperino'];
+console.log('arrayNomi è composto da: ' + arrayNomi);
+var posizioneMin;
+var posizioneMax;
 
-// ECMA5
-// var arrayNomi = ['qui', 'quo', 'qua', 'pippo', 'pluto', 'paperino'];
-// console.log('arrayNomi è composto da: ' + arrayNomi);
-//
-// var posizioneMin = parseInt(prompt('Inserisci il numero min'));
-// var posizioneMax = parseInt(prompt('Inserisci il numero max (non superiore a 6)'));
-//
-// function myFunction(array, min, max) { //dove min e max rappresentano la posizione umana nell'array (+1 rispetto all'index)
+// *****funzioni*****
+function minNotValid() {
+  return isNaN(posizioneMin) || posizioneMin <= 0 || posizioneMin > arrayNomi.length;
+}
+
+function maxNotValid() {
+  return isNaN(posizioneMax) || posizioneMax > arrayNomi.length || posizioneMax <= posizioneMin;
+}
+
+// *****ECMA5*****
+// function arrayFilter(array, min, max) { //dove min e max rappresentano la posizione umana nell'array (+1 rispetto all'index)
 //   var newArray = [];
 //   for (var x = 0; x < array.length; x++) {
 //     if ((x + 1) >= min && (x + 1) <= max) {
@@ -17,17 +23,25 @@
 //   }
 //   return newArray;
 // };
-//
-// console.log('I nomi che hanno posizione compresa tra ' + posizioneMin + ' e ' + posizioneMax + ' di arrayNomi sono: ' + myFunction(arrayNomi, posizioneMin, posizioneMax));
-//
+
+posizioneMin = parseInt(prompt('Inserisci il numero minimo'));
+// controllo posizioneMin
+while (minNotValid()) {
+  alert('Inserisci un numero minimo valido');
+  posizioneMin = parseInt(prompt('Inserisci il numero minimo'));
+}
+
+posizioneMax = parseInt(prompt('Inserisci il numero massimo (non superiore a 6)'));
+// controllo posizioneMax
+while (maxNotValid()) {
+  alert('Inserisci un numero massimo valido');
+  posizioneMax = parseInt(prompt('Inserisci il numero massimo (non superiore a 6)'));
+};
+
+// console.log('I nomi con posizione compresa tra ' + posizioneMin + ' e ' + posizioneMax + ' sono: ' + arrayFilter(arrayNomi, posizioneMin, posizioneMax));
+
 // ECMA6
-const arrayNomi = ['qui', 'quo', 'qua', 'pippo', 'pluto', 'paperino'];
-console.log('arrayNomi è composto da: ' + arrayNomi);
-
-let posizioneMin = parseInt(prompt('Inserisci il numero min'));
-let posizioneMax = parseInt(prompt('Inserisci il numero max (non superiore a 6'));
-
-let myFunction = (array, min, max) => {
+function arrayFilter(array, min, max) {
   let newArray = array.filter((element, index, array) => {
     return (min <= (index + 1) && (index + 1) <= max)
   });
@@ -35,6 +49,6 @@ let myFunction = (array, min, max) => {
 }
 
 console.log(
-  `I nomi che hanno posizione compresa tra ${posizioneMin} e ${posizioneMax} di arrayNomi sono `
-  + myFunction(arrayNomi, posizioneMin, posizioneMax)
+  `I nomi con posizione compresa tra ${posizioneMin} e ${posizioneMax} sono `
+  + arrayFilter(arrayNomi, posizioneMin, posizioneMax)
 );
